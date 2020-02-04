a7d0df0f851b7d57591d087d5f89e3e505a5cdb6
johnduffy89 <johnduffy@amdg.rockhursths.edu>
2020-02-04 08:45:59 -0600

    some code

---
+++ hellogit.py
@@ -0,0 +1,52 @@
+# comment
+
+print("Hello World!")
+a = 6
+b = 5
+print(a + b)
+
+#how to make a list
+myList = [1, 2, 3, 4, 3, 2, 1]
+
+#how to access elements of a list
+print(myList[0:6:2]) # from index 0 to 6, skip by 2
+
+phrase = 'Catch the Dog'
+phrase[2] # t
+phrase[4] # h
+phrase[-1:0:-3]
+
+#how to write a function
+def sumFunction(a, b):
+	return a + b
+
+#how to call a function
+print(sumFunction(2, 20))
+
+x = 10
+y = -10
+
+if(x==10)
+	x = 5
+elif(y== -10)
+	y = 5;
+else: x = y - x
+
+
+
+
+mylist = [1, 3, 8, 412, 43, 2, 20]
+len(mylist)
+
+
+for i in range(len(mylist));
+	print9mylist[i]))
+
+for val in mylist
+	print (val)
+lookup = {}
+lookup{'kc'} = 'chiefs'
+lookup{'ne'} = 'patriots'
+lookup{'la'} = 'chargers'
+lookup{'suck'} = 'raiders'
+
